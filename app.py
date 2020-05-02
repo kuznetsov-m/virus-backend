d657aae9803ad4ab9a9094cd3771677992fbb59c
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2020-05-02 17:32:27 +0300

    Added start files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request, render_template
+from sys import argv
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Test page</h1>"
+
+if __name__ == "__main__":
+
+    if len(argv) == 2:
+        app.run(host='0.0.0.0', port=int(argv[1]))
+    else:
+        app.run(port=8080) 
