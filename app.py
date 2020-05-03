f2cd67218e595247c610829a9af4de5fd49c98ec
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2020-05-03 12:26:40 +0300

    Update

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ if __name__ == "__main__":
     if len(argv) == 2:
         app.run(host='0.0.0.0', port=int(argv[1]))
     else:
-        app.run(port=8080, debug=True) 
+        app.run(port=8080, debug=True)
